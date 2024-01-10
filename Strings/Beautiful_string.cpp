8369e46c9f86abf93f98e16ac4784deed6ae3734
Rhythm Mahale <88775908+Rpm2002@users.noreply.github.com>
2024-01-10 04:21:20 +0000

    beautiful string - codestudio

---
+++ Strings/Beautiful_string.cpp
@@ -0,0 +1,86 @@
+/*
+
+Ninja has been given a binary string ‘STR’ containing either ‘0’ or ‘1’. A binary string is called beautiful 
+if it contains alternating 0s and 1s.
+For Example:‘0101’, ‘1010’, ‘101’, ‘010’ are beautiful strings.
+He wants to make ‘STR’ beautiful by performing some operations on it. In one operation, Ninja can convert ‘0’ 
+into ‘1’ or vice versa.
+Your task is to determine the minimum number of operations Ninja should perform to make ‘STR’ beautiful.
+
+2 // Test cases
+0000
+1010
+Sample Output 1 :
+2
+0
+
+*/
+
+// *************************************** CODE ***************************************
+
+/*
+
+int makeBeautiful(string str) 
+{
+	int i=0,n=str.length();
+	int count1=0;
+	int count2=0;
+	string str1="";
+	string str2="";
+
+	// Pehli pattern generate karo
+
+	for(int i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			str1+='0';
+		}
+
+		else
+		{
+			str1+='1';
+		}
+	}
+
+	// // dusri pattern generate karo
+
+	for(int i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			str2+='1';
+		}
+
+		else
+		{
+			str2+='0';
+		}
+	}
+
+	// ab compare karo changes aur minimum changes jisme ho wo count return karo
+
+	for(int i=0;i<n;i++)
+	{
+		if(str[i]!=str1[i])
+		{
+			count1++;
+		}
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		if(str[i]!=str2[i])
+		{
+			count2++;
+		}
+	}
+
+	return min(count1,count2);
+
+}
+
+
+// Youtube : https://youtu.be/P1hgy238oPU?feature=shared
+
+*/
